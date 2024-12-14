6e4a7267492f36c35102847827cf35377cf2daee
Leo <alvarezleandro720@gmail.com>
2024-12-13 19:53:47 -0500

    commented middleware for vercel deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,26 +6,26 @@ import type { NextRequest } from "next/server";
 import { Redis } from '@upstash/redis'
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 
-const redis = Redis.fromEnv();
+// const redis = Redis.fromEnv();
 
-// Define the rate limiter for fixed window (10 requests per 10 seconds)
-const ratelimit= new Ratelimit({
-  redis, // Use Redis instance
-  limiter: Ratelimit.slidingWindow(`0`, "10 s"), // 10 requests per 10 seconds
-  analytics: true, // Optional: Enable analytics if needed
-});
-//this runs before home page
-export async function middleware(req: NextRequest) {
-  // const ip  = req.headers.get('x-forwarded-for') ?? '127.0.0.1'
-  // console.log(ip)
-  // const {success, remaining, reset} = await ratelimit.limit(ip);
-  // if (!success) {
-  //   console.log('RATE LIMIT EXCEEDED')
-  //   return NextResponse.json('Rate Limit Exceeded', { status: 429 });
-  // }
+// // Define the rate limiter for fixed window (10 requests per 10 seconds)
+// const ratelimit= new Ratelimit({
+//   redis, // Use Redis instance
+//   limiter: Ratelimit.slidingWindow(`0`, "10 s"), // 10 requests per 10 seconds
+//   analytics: true, // Optional: Enable analytics if needed
+// });
+// //this runs before home page
+// export async function middleware(req: NextRequest) {
+//   // const ip  = req.headers.get('x-forwarded-for') ?? '127.0.0.1'
+//   // console.log(ip)
+//   // const {success, remaining, reset} = await ratelimit.limit(ip);
+//   // if (!success) {
+//   //   console.log('RATE LIMIT EXCEEDED')
+//   //   return NextResponse.json('Rate Limit Exceeded', { status: 429 });
+//   // }
   
-  // return NextResponse.json('Rate Limit Not Exceeded', { status: 200 });
-}
+//   // return NextResponse.json('Rate Limit Not Exceeded', { status: 200 });
+// }
 
 
 // Configure which paths the middleware runs on
